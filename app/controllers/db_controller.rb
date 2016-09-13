19e4e039b3560cd58595363daa161eecfe65dae4
Allen Ballman <aballman@gmail.com>
2016-09-13 10:29:57 -0400

    Make all calls use the proper url.

--- app/controllers/db_controller.rb
+++ app/controllers/db_controller.rb
@@ -1,6 +1,6 @@
 class DbController < ApplicationController
 
-  HOST = ENV['AG_WEATHER_HOST'] || 'http://localhost:3000'
+  HOST = ENV['AG_WEATHER_BASE_URL'] || 'http://localhost:3000'
 
   def severities
     start_date = params[:start_date]
@@ -22,8 +22,8 @@ class DbController < ApplicationController
     @latitude = params[:latitude].to_f.round(1)
     @longitude = params[:longitude].to_f.round(1)
     url = crop == 'carrot' ?
-      "http://localhost:4000/carrot_forecasts/info?start_date=#{start_date}&end_date=#{end_date}&longitude=#{@longitude}&latitude=#{@latitude}" :
-      "http://localhost:4000/potato_forecasts/info?start_date=#{start_date}&end_date=#{end_date}&longitude=#{@longitude}&latitude=#{@latitude}"
+      "#{HOST}/carrot_forecasts/info?start_date=#{start_date}&end_date=#{end_date}&longitude=#{@longitude}&latitude=#{@latitude}" :
+      "#{HOST}/potato_forecasts/info?start_date=#{start_date}&end_date=#{end_date}&longitude=#{@longitude}&latitude=#{@latitude}"
 
     response = HTTParty.get(url, { timeout: 10 })
     @weather = JSON.parse(response.body)
