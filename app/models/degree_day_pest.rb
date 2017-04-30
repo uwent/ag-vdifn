40bc7aa0a7360af74f7b7f51e1ebe77f5a80a836
Allen Ballman <aballman@gmail.com>
2017-04-30 12:58:21 -0400

    Added new models for degree days.

---
+++ app/models/degree_day_pest.rb
@@ -0,0 +1,33 @@
+class DegreeDayPest < Pest
+
+  def severities(start_date, end_date)
+    url = "#{REMOTE_HOST}/degree_days/totals?start_date=#{start_date}&end_date=#{end_date}&pest=#{remote_name}"
+
+    begin
+      response = HTTParty.get(url, { timeout: 10 })
+      return severities_from_totals(JSON.parse(response.body))
+    rescue Exception => e
+      logger.error(e.backtrace.join("\n"))
+      return []
+    end
+
+  end
+
+  private
+  def total_to_severity(total)
+    return 5 if (total >= critical_value * 0.95 && total <= critical_value * 1.05)
+    return 4 if (total >= critical_value * 0.90 && total <= critical_value * 1.1)
+    return 3 if (total >= critical_value * 0.85 && total <= critical_value * 1.15)
+    return 2 if (total >= critical_value * 0.80 && total <= critical_value * 1.2)
+    return 1
+  end
+
+  def severities_from_totals(totals)
+    totals.map do | measurement |
+      { latitude: measurement['latitude'],
+        longitude: measurement['longitude'],
+        severity: total_to_severity(measurement['total'].to_f)
+      }
+    end
+  end
+end
--- app/models/pest.rb
+++ app/models/pest.rb
@@ -1,4 +1,5 @@
 class Pest < ActiveRecord::Base
+  REMOTE_HOST = ENV['AG_WEATHER_BASE_URL'] || 'http://localhost:3000'
 
   has_many :crop_pests, dependent: :destroy
   has_many :crops, through: :crop_pests
