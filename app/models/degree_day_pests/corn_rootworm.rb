dd1f8c732e6ed3e8ef5ca74a4b354d384f5cc26b
Allen Ballman <aballman@gmail.com>
2017-05-11 15:32:24 -0400

    Forgot the class file.

---
+++ app/models/degree_day_pests/corn_rootworm.rb
@@ -0,0 +1,34 @@
+class CornRootworm < DegreeDayPest
+
+  def total_to_severity(total)
+    return 1 if total < 263 || total > 566
+    return 2 if total > 543
+    return 3 if total > 519
+    return 4 if total > 495
+    return 5 if total > 448
+    return 4 if total > 408
+    return 5 if total > 369
+    return 4 if total > 345
+    return 5 if total > 312
+    return 4 if total > 296
+    return 3 if total > 279
+    return 2
+  end
+
+  def severity_legend
+    [
+      {name: "Very High", slug: "very_high",
+       description: "Very high likelihood (very near peak 1st instar, peak 2nd instar, or peak 3rd instar)"},
+      {name: "High", slug: "high",
+       description: "High likelihood (near peak 1st instar, peak 2nd instar, or peak 3rd instar)"},
+      {name: "Medium", slug: "medium",
+       description: "Medium likelihood (before peak 1st instar or after 2nd instar)"},
+      {name: "Low", slug: "low",
+       description: "Low likelihood (well before peak 1st instar or after 2nd instar)"},
+      {name: "Very Low", slug: "very_low",
+       description: "Very low likelihood (not near peak 1st instar, peak 2nd instar, or peak 3rd instar)"}
+    ]
+  end
+
+
+end
