fb2d3add4c89eb38aaaacc6ad2278971699ca67b
Ben Bradford <bradfordium@gmail.com>
2021-03-09 10:40:35 -0600

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,10 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module RailsApp
+module AgVDIFN
   class Application < Rails::Application
-    config.load_defaults "6.0"
+    config.load_defaults 6.0
+    config.time_zone = 'Central Time (US & Canada)'
     config.eager_load_paths += %W(#{Rails.root}/app/models/dsv_pests)
     config.eager_load_paths += %W(#{Rails.root}/app/models/degree_day_pests)
   end
