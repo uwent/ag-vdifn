27fdcc624f7a4bec2e567ed9e12e186c081add8b
Ben Bradford <bradfordium@gmail.com>
2021-04-21 17:00:54 -0500

    update logger levels

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :debug
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
