386598d1f93e00ca75788fd866bbfd30ce001c57
Ben Bradford <bradfordium@gmail.com>
2021-04-27 13:27:45 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,9 +63,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'db:seed'
+      end
     end
   end
 
