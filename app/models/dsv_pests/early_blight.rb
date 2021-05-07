6d56815112d7a6413baf45191375ac9abeb17107
Ben Bradford <bradfordium@gmail.com>
2021-05-07 11:56:54 -0500

    Update early_blight.rb

--- app/models/dsv_pests/early_blight.rb
+++ app/models/dsv_pests/early_blight.rb
@@ -1,31 +1,29 @@
 class EarlyBlight < DsvPest
 
-  def severities_from_totals(selected_dates, past_7_days, past_2_days)
-    selected_dates.zip(past_7_days).map do | pair |
+  def severities_from_totals(selected_dates, last_7_days, last_2_days)
+    selected_dates.zip(last_7_days).map do | pair |
       {
         lat: pair[0][:lat],
         long: pair[0][:long],
-        severity: total_to_severity(
-          pair[0][:total],
-          pair[1][:total]
-        )
+        severity: total_to_severity(pair[0][:total], pair[1][:total])
       }
     end
   end
 
-  def total_to_severity(selected_dates, past_7_days)
-    if selected_dates > 300
-      daily_avg = past_7_days / 7
-      return 4 if daily_avg > 8
-      return 3 if daily_avg > 5
-      return 2 if daily_avg > 3
-      return 1 if daily_avg > 1
+  def total_to_severity(selected_dates, last_7_days)
+    if selected_dates >= 300
+      avg = last_7_days / 7
+      return 4 if avg >= 8
+      return 3 if avg >= 5
+      return 2 if avg >= 3
+      return 1 if avg >= 1
+      return 0
+    else
+      return 3 if selected_dates >= 250
+      return 2 if selected_dates >= 200
+      return 1 if selected_dates >= 150
       return 0
     end
-    return 3 if selected_dates > 250
-    return 2 if selected_dates > 200
-    return 1 if selected_dates > 150
-    return 0
   end
 
   def severity_legend
