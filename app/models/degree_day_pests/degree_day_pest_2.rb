13107be76543fd8df501f4dd3854cff237da8183
Ben Bradford <bradfordium@gmail.com>
2021-05-07 11:56:33 -0500

    fix model errors

--- app/models/degree_day_pests/degree_day_pest_2.rb
+++ app/models/degree_day_pests/degree_day_pest_2.rb
@@ -1,7 +1,7 @@
 # pests with two generations per year
 class DegreeDayPest2 < DegreeDayPest
 
-  def total_to_severity(total, freezing)
+  def total_to_severity(total, freezing, end_date)
     return 0 if freezing
     return sev_ramp(risk_start, risk_peak, risk_end, total) if total.between?(risk_start, risk_end)
     return sev_ramp(risk_start2, risk_peak2, risk_end2, total) if total.between?(risk_start2, risk_end2)
--- app/models/degree_day_pests/seedcorn_maggot.rb
+++ app/models/degree_day_pests/seedcorn_maggot.rb
@@ -1,6 +1,6 @@
 class SeedcornMaggot < DegreeDayPest
 
-  def total_to_severity(total, freezing)
+  def total_to_severity(total, freezing, end_date)
     return 0 if freezing
     return sev_ramp(risk_start, risk_peak, risk_end, total) if total.between?(risk_start, risk_end)
     return sev_ramp(risk_start2, risk_peak2, risk_end2, total) if total.between?(risk_start2, risk_end2)
