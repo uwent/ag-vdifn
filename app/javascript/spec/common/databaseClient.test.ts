52a13f27bb666ece81f77a0cfd71b3350cdb62cf
Ben Bradford <bradfordium@gmail.com>
2021-05-18 13:31:55 -0500

    refactor and change biofix_date to use .toDateString()

--- app/javascript/spec/common/databaseClient.test.ts
+++ app/javascript/spec/common/databaseClient.test.ts
@@ -1,12 +1,12 @@
-import DatabaseClient from '../../src/components/common/TypeScript/databaseClient'
+import DatabaseClient from "../../src/components/common/TypeScript/databaseClient"
 import {
   PointDetailsParams,
   SeverityParams,
-  StationDetailsParams,
-} from '../../src/components/common/TypeScript/types'
+  // StationDetailsParams,
+} from "../../src/components/common/TypeScript/types"
 
-describe('fetchSeverities', () => {
-  it('calls correct url and returns data', async () => {
+describe("fetchSeverities", () => {
+  it("calls correct url and returns data", async () => {
     const start_date = new Date().toDateString()
     const end_date = new Date().toDateString()
     const pest_id = 1
@@ -21,7 +21,7 @@ describe('fetchSeverities', () => {
     ])
   })
 
-  it('returns empty list if call fails', async () => {
+  it("returns empty list if call fails", async () => {
     const start_date = new Date().toDateString()
     const end_date = new Date().toDateString()
     const pest_id = 101
@@ -34,32 +34,32 @@ describe('fetchSeverities', () => {
   })
 })
 
-describe('fetchSeverityLegend', () => {
-  it('calls correct url and returns data', async () => {
+describe("fetchSeverityLegend", () => {
+  it("calls correct url and returns data", async () => {
     const database = new DatabaseClient()
     const pestId = 1
     const response = await database.fetchSeverityLegend(pestId)
 
     expect(response).toEqual([
       {
-        name: 'High',
-        slug: 'very_high',
-        description: 'High likelihood of disease\n(widespread outbreak observed OR 7-day accumulated DSVs ≥ 21 or isolated outbreak observed)',
+        name: "High",
+        slug: "very_high",
+        description: "High likelihood of disease\n(widespread outbreak observed OR 7-day accumulated DSVs ≥ 21 or isolated outbreak observed)",
       },
       {
-        name: 'Medium',
-        slug: 'medium',
-        description: 'Medium likelihood of disease\n(7-day accumulated DSVs ≥ 3 or season accumulated DSVs \u003e 30)',
+        name: "Medium",
+        slug: "medium",
+        description: "Medium likelihood of disease\n(7-day accumulated DSVs ≥ 3 or season accumulated DSVs \u003e 30)",
       },
       {
-        name: 'Low',
-        slug: 'very_low',
-        description: 'Low likelihood of disease\n(7-day accumulated DSVs ≤ 3 and season accumulated DSVs \u003c 30)',
+        name: "Low",
+        slug: "very_low",
+        description: "Low likelihood of disease\n(7-day accumulated DSVs ≤ 3 and season accumulated DSVs \u003c 30)",
       },
     ])
   })
 
-  it('returns empty list on failure', async () => {
+  it("returns empty list on failure", async () => {
     const database = new DatabaseClient()
     const pestId = 101
 
@@ -69,33 +69,32 @@ describe('fetchSeverityLegend', () => {
   })
 })
 
-describe('fetchSeverityLegendInfo', () => {
-  it('calls correct url and returns data', async () => {
+describe("fetchSeverityLegendInfo", () => {
+  it("calls correct url and returns data", async () => {
     const database = new DatabaseClient()
     const pestId = 1
     const response = await database.fetchSeverityLegendInfo(pestId)
-    console.log(response)
-    expect(response).toEqual('Alfalfa Weevil Info')
+    expect(response).toEqual("Alfalfa Weevil Info")
   })
 
-  it('returns empty string failure', async () => {
+  it("returns empty string failure", async () => {
     const database = new DatabaseClient()
     const pestId = 101
     const response = await database.fetchSeverityLegendInfo(pestId)
 
-    expect(response).toEqual('')
+    expect(response).toEqual("")
   })
 })
 
-describe('fetchPointDetails', () => {
-  it('calls correct url and returns data', async () => {
+describe("fetchPointDetails", () => {
+  it("calls correct url and returns data", async () => {
     const database = new DatabaseClient()
     const latitude = 100
     const longitude = 200
     const start_date = new Date().toDateString()
     const end_date = new Date().toDateString()
     const pest_id = 1
-    const panel = 'disease'
+    const panel = "disease"
     const params: PointDetailsParams = {
       latitude,
       longitude,
@@ -107,17 +106,17 @@ describe('fetchPointDetails', () => {
 
     const response = await database.fetchPointDetails(params)
 
-    expect(response).toEqual('string')
+    expect(response).toEqual("string")
   })
 
-  it('returns empty string on failure', async () => {
+  it("returns empty string on failure", async () => {
     const database = new DatabaseClient()
     const latitude = 100
     const longitude = 200
     const start_date = new Date().toDateString()
     const end_date = new Date().toDateString()
     const pest_id = 101
-    const panel = 'disease'
+    const panel = "disease"
     const params: PointDetailsParams = {
       latitude,
       longitude,
@@ -129,12 +128,12 @@ describe('fetchPointDetails', () => {
 
     const response = await database.fetchPointDetails(params)
 
-    expect(response).toEqual('')
+    expect(response).toEqual("")
   })
 })
 
-describe('fetchPestInfo', () => {
-  it('calls correct url and returns data', async () => {
+describe("fetchPestInfo", () => {
+  it("calls correct url and returns data", async () => {
     const database = new DatabaseClient()
     const pestId = 1
     const inFahrenheit = true
@@ -142,17 +141,17 @@ describe('fetchPestInfo', () => {
     const response = await database.fetchPestInfo(pestId, inFahrenheit)
 
     expect(response).toEqual({
-      info: 'info',
-      name: 'pest name',
-      pest_link: 'www.example.com',
-      biofix_date: new Date().toString(),
+      info: "info",
+      name: "pest name",
+      pest_link: "www.example.com",
+      biofix_date: new Date().toDateString(),
       end_date_enabled: true,
       tmin: 0,
       tmax: 100,
     })
   })
 
-  it('returns empty object on failure', async () => {
+  it("returns empty object on failure", async () => {
     const database = new DatabaseClient()
     const pestId = 101
     const inFahrenheit = true
@@ -171,34 +170,34 @@ describe('fetchPestInfo', () => {
   })
 })
 
-// describe('fetchStationDetails', () => {
-//   it('calls correct url and returns data', async () => {
+// describe("fetchStationDetails", () => {
+//   it("calls correct url and returns data", async () => {
 //     const database = new DatabaseClient()
-//     const name = 'name'
+//     const name = "name"
 //     const start_date = new Date()
 //     const end_date = new Date()
 //     const params: StationDetailsParams = { name, start_date, end_date }
 
 //     const response = await database.fetchStationDetails(params)
 
-//     expect(response).toEqual('string')
+//     expect(response).toEqual("string")
 //   })
 
-//   it('returns empty string on failure', async () => {
+//   it("returns empty string on failure", async () => {
 //     const database = new DatabaseClient()
-//     const name = 'error'
+//     const name = "error"
 //     const start_date = new Date()
 //     const end_date = new Date()
 //     const params: StationDetailsParams = { name, start_date, end_date }
 
 //     const response = await database.fetchStationDetails(params)
 
-//     expect(response).toEqual('')
+//     expect(response).toEqual("")
 //   })
 // })
 
-describe('fetchDiseasePanel', () => {
-  it('converts diseases into afflictions', async () => {
+describe("fetchDiseasePanel", () => {
+  it("converts diseases into afflictions", async () => {
     const database = new DatabaseClient()
 
     const response = await database.fetchDiseasePanel()
@@ -206,15 +205,15 @@ describe('fetchDiseasePanel', () => {
     expect(response).toEqual([
       {
         id: 1,
-        name: 'potatoes',
-        afflictions: [{ id: 2, name: 'late blight' }],
+        name: "potatoes",
+        afflictions: [{ id: 2, name: "late blight" }],
       },
     ])
   })
 })
 
-describe('fetchInsectPanel', () => {
-  it('converts diseases into afflictions', async () => {
+describe("fetchInsectPanel", () => {
+  it("converts diseases into afflictions", async () => {
     const database = new DatabaseClient()
 
     const response = await database.fetchInsectPanel()
@@ -222,20 +221,20 @@ describe('fetchInsectPanel', () => {
     expect(response).toEqual([
       {
         id: 1,
-        name: 'potato',
+        name: "potato",
         afflictions: [
           {
             id: 10,
-            name: 'grasshopper',
-            biofix_date: '2020-10-10',
+            name: "grasshopper",
+            biofix_date: "2020-10-10",
             end_date_enabled: true,
             t_min: 2,
             t_max: 3,
           },
           {
             id: 45,
-            name: 'caterpillar',
-            biofix_date: '2020-11-10',
+            name: "caterpillar",
+            biofix_date: "2020-11-10",
             end_date_enabled: false,
             t_min: 4,
             t_max: 5,
--- app/javascript/spec/msw_mocks/handlers.js
+++ app/javascript/spec/msw_mocks/handlers.js
@@ -1,140 +1,164 @@
-import { rest } from 'msw'
-import ENDPOINTS from '../../src/components/common/TypeScript/endpoints';
+import { rest } from "msw"
+import ENDPOINTS from "../../src/components/common/TypeScript/endpoints"
 
 export const handlers = [
-    rest.get(ENDPOINTS.DISEASE_PANEL, (req, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json([
-                { id: 1, name: 'potatoes', diseases: [{ id: 2, name: 'late blight' }] }
-            ])
-        )
-    }),
+  rest.get(ENDPOINTS.DISEASE_PANEL, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { id: 1, name: "potatoes", diseases: [{ id: 2, name: "late blight" }] },
+      ]),
+    )
+  }),
 
-    rest.post(ENDPOINTS.SEVERITIES, (req, res, ctx) => {
-        const { pest_id } = req.body
-        if (pest_id === 101) {
-            return res(
-                ctx.status(500)
-            )
-        } else {
-            return res(
-                ctx.status(200),
-                ctx.json([
-                    { "lat": 5, "long": 10, "severity": 10 },
-                    { "lat": 50, "long": 60, "severity": 5 }
-                ])
-            )
-        }
-    }),
+  rest.post(ENDPOINTS.SEVERITIES, (req, res, ctx) => {
+    const { pest_id } = req.body
+    if (pest_id === 101) {
+      return res(ctx.status(500))
+    } else {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          { lat: 5, long: 10, severity: 10 },
+          { lat: 50, long: 60, severity: 5 },
+        ]),
+      )
+    }
+  }),
 
-    rest.post(ENDPOINTS.SEVERITY_LEGEND, (req, res, ctx) => {
-        const { pest_id } = req.body
-        if (pest_id === 101) {
-            return res(
-                ctx.status(500)
-            )
-        } else {
-            return res(
-                ctx.status(200),
-                ctx.json([
-                    { "name": "High", "slug": "very_high", "description": "High likelihood of disease\n(widespread outbreak observed OR 7-day accumulated DSVs ≥ 21 or isolated outbreak observed)" },
-                    { "name": "Medium", "slug": "medium", "description": "Medium likelihood of disease\n(7-day accumulated DSVs ≥ 3 or season accumulated DSVs \u003e 30)" },
-                    { "name": "Low", "slug": "very_low", "description": "Low likelihood of disease\n(7-day accumulated DSVs ≤ 3 and season accumulated DSVs \u003c 30)" }
-                ])
-            )
-        }
-    }),
+  rest.post(ENDPOINTS.SEVERITY_LEGEND, (req, res, ctx) => {
+    const { pest_id } = req.body
+    if (pest_id === 101) {
+      return res(ctx.status(500))
+    } else {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            name: "High",
+            slug: "very_high",
+            description:
+              "High likelihood of disease\n(widespread outbreak observed OR 7-day accumulated DSVs ≥ 21 or isolated outbreak observed)",
+          },
+          {
+            name: "Medium",
+            slug: "medium",
+            description:
+              "Medium likelihood of disease\n(7-day accumulated DSVs ≥ 3 or season accumulated DSVs \u003e 30)",
+          },
+          {
+            name: "Low",
+            slug: "very_low",
+            description:
+              "Low likelihood of disease\n(7-day accumulated DSVs ≤ 3 and season accumulated DSVs \u003c 30)",
+          },
+        ]),
+      )
+    }
+  }),
 
-    rest.post(ENDPOINTS.SEVERITY_LEGEND_INFO, (req, res, ctx) => {
-        const { pest_id } = req.body
-        if (pest_id === 101) {
-            return res(
-                ctx.status(500)
-            )
-        } else {
-            return res(
-                ctx.status(200),
-                ctx.json("Alfalfa Weevil Info")
-            )
-        }
-    }),
+  rest.post(ENDPOINTS.SEVERITY_LEGEND_INFO, (req, res, ctx) => {
+    const { pest_id } = req.body
+    if (pest_id === 101) {
+      return res(ctx.status(500))
+    } else {
+      return res(ctx.status(200), ctx.json("Alfalfa Weevil Info"))
+    }
+  }),
 
-    rest.post(ENDPOINTS.POINT_DETAILS, (req, res, ctx) => {
-        const { pest_id } = req.body
-        if (pest_id === 101) {
-            return res(
-                ctx.status(500)
-            )
-        } else {
-            return res(
-                ctx.status(200),
-                ctx.json("string")
-            )
-        }
-    }),
+  rest.post(ENDPOINTS.POINT_DETAILS, (req, res, ctx) => {
+    const { pest_id } = req.body
+    if (pest_id === 101) {
+      return res(ctx.status(500))
+    } else {
+      return res(ctx.status(200), ctx.json("string"))
+    }
+  }),
 
-    rest.post(ENDPOINTS.PEST_INFO, (req, res, ctx) => {
-        const { pest_id } = req.body
-        if (pest_id === 101) {
-            return res(
-                ctx.status(500)
-            )
-        } else {
-            return res(
-                ctx.status(200),
-                ctx.json({
-                    info: "info",
-                    name: "pest name",
-                    pest_link: "www.example.com",
-                    biofix_date: new Date().toString(),
-                    end_date_enabled: true,
-                    tmin: 0,
-                    tmax: 100
-                })
-            )
-        }
-    }),
+  rest.post(ENDPOINTS.PEST_INFO, (req, res, ctx) => {
+    const { pest_id } = req.body
+    if (pest_id === 101) {
+      return res(ctx.status(500))
+    } else {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          info: "info",
+          name: "pest name",
+          pest_link: "www.example.com",
+          biofix_date: new Date().toDateString(),
+          end_date_enabled: true,
+          tmin: 0,
+          tmax: 100,
+        }),
+      )
+    }
+  }),
 
-    rest.post(ENDPOINTS.STATION_DETAILS, (req, res, ctx) => {
-        const { name } = req.body
-        if (name === 'error') {
-            return res(
-                ctx.status(500)
-            )
-        } else {
-            return res(
-                ctx.status(200),
-                ctx.json("string")
-            )
-        }
-    }),
+  rest.post(ENDPOINTS.STATION_DETAILS, (req, res, ctx) => {
+    const { name } = req.body
+    if (name === "error") {
+      return res(ctx.status(500))
+    } else {
+      return res(ctx.status(200), ctx.json("string"))
+    }
+  }),
 
-    rest.get(ENDPOINTS.DISEASE_PANEL, (req, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json([
-                {
-                    id: 1, name: "potato", diseases: [
-                        { id: 5, name: "late blight", t_min: null, t_max: null, end_date_enabled: true },
-                        { id: 10, name: "black death", t_min: null, t_max: null, end_date_enabled: true }
-                    ]
-                }
-            ])
-        )
-    }),
+  rest.get(ENDPOINTS.DISEASE_PANEL, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          name: "potato",
+          diseases: [
+            {
+              id: 5,
+              name: "late blight",
+              t_min: null,
+              t_max: null,
+              end_date_enabled: true,
+            },
+            {
+              id: 10,
+              name: "black death",
+              t_min: null,
+              t_max: null,
+              end_date_enabled: true,
+            },
+          ],
+        },
+      ]),
+    )
+  }),
 
-    rest.get(ENDPOINTS.INSECT_PANEL, (req, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json([
-                {
-                    id: 1, name: "potato", insects: [
-                        { id: 10, name: "grasshopper", biofix_date: "2020-10-10", end_date_enabled: true, t_min: 2, t_max: 3 },
-                        { id: 45, name: "caterpillar", biofix_date: "2020-11-10", end_date_enabled: false, t_min: 4, t_max: 5,  }
-                    ]
-                }
-            ])
-        )
-    })
+  rest.get(ENDPOINTS.INSECT_PANEL, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          name: "potato",
+          insects: [
+            {
+              id: 10,
+              name: "grasshopper",
+              biofix_date: "2020-10-10",
+              end_date_enabled: true,
+              t_min: 2,
+              t_max: 3,
+            },
+            {
+              id: 45,
+              name: "caterpillar",
+              biofix_date: "2020-11-10",
+              end_date_enabled: false,
+              t_min: 4,
+              t_max: 5,
+            },
+          ],
+        },
+      ]),
+    )
+  }),
 ]
