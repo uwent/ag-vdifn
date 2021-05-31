e9ee1b4a80ca62003bd6b5c07bd4cd6225153df0
Ben Bradford <bradfordium@gmail.com>
2021-05-31 10:33:05 -0500

    fix webpack merge problem

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,21 +1,11 @@
-const { webpackConfig, merge } = require('@rails/webpacker');
+const { webpackConfig } = require('@rails/webpacker');
+const { merge } = require('webpack-merge');
 const svelteConfig = require('./loaders/svelte');
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const path = require("path");
 
-// const env = require('@rails/webpacker/package/env');
-// const webpack = require('webpack')
-
-// environment.loaders.prepend('svelte', svelte)
 module.exports = merge(
   webpackConfig,
   svelteConfig,
-  {
-    plugins: [new ForkTsCheckerWebpackPlugin()],
-  },
-  {
-    resolve: {
-      extensions: ['.css']
-    }
-  }
+  { plugins: [new ForkTsCheckerWebpackPlugin()] },
+  { resolve: { extensions: ['.css'] } }
 );
