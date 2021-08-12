b9b044e21d4533482bf789da5f00f3ed81a46648
Ben Bradford <bradfordium@gmail.com>
2021-08-11 19:20:22 -0500

    update configs

--- config/webpack/staging.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-process.env.WEBPACKER_ASSET_HOST = '/vdifn/'
-
-const webpackConfig = require('./base')
-
-module.exports = webpackConfig
