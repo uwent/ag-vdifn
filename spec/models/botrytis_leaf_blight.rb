b2657c034a2093431240e4388c70883e6290170c
Ben Bradford <bradfordium@gmail.com>
2022-02-28 14:16:02 -0600

    add test for botrytis

---
+++ spec/models/botrytis_leaf_blight.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+RSpec.describe BotrytisLeafBlight, type: :model do
+  let(:biofix) { Date.today - 14.days }
+  let(:pest) { BotrytisLeafBlight.new }
+  let(:grid) {
+    [
+      {lat: 1, long: 1, total: rand(100)},
+      {lat: 1, long: 2, total: rand(100)},
+      {lat: 2, long: 1, total: rand(100)},
+      {lat: 2, long: 2, total: rand(100)}
+    ]
+  }
+
+  it "has a severity legend with 5 levels" do
+    legend = pest.severity_legend
+    expect(legend).not_to be_empty
+    expect(legend.length).to eq(5)
+  end
+
+  it "renders severity from totals" do
+    expect(pest.total_to_severity(0)).to eq(0)
+    expect(pest.total_to_severity(1)).to eq(1)
+    expect(pest.total_to_severity(21)).to eq(2)
+    expect(pest.total_to_severity(31)).to eq(3)
+    expect(pest.total_to_severity(40))).to eq(4)
+  end
+
+  it "generates severities from weather" do
+    expect(pest).to receive(:total_to_severity).exactly(4).times
+    pest.severities_from_totals(grid)
+  end
+
+  it "sets biofix date" do
+    expect(pest.biofix_date).to eq(biofix)
+  end
+end
