24c0220dc455469f67deefdb4c74fe0ed4cb7dcf
Ben Bradford <bradfordium@gmail.com>
2022-02-28 20:16:09 -0600

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :deploy do
   task :yarn_install do
     on roles(:app) do
       within release_path do
-        execute "yarn install --silent"
+        execute "cd #{release_path} && yarn install --production --silent"
       end
     end
   end
