9aa2e79582c7d0f173c0c858bf344e772424c7d6
Ben Bradford <bradfordium@gmail.com>
2022-03-04 14:20:03 -0600

    update oak wilt language

--- app/models/insects/oak_wilt.rb
+++ app/models/insects/oak_wilt.rb
@@ -35,11 +35,11 @@ class OakWilt < Insect
 
   def severity_legend
     [
-      {name: "Very High", slug: "very_high", description: "Very high risk of oak wilt (25-75% beetle flight)"},
-      {name: "High", slug: "high", description: "High risk of oak wilt (10-25% or 75-90% beetle flight)"},
-      {name: "Medium", slug: "medium", description: "Medium risk of oak wilt (5-10% or 90-95% beetle flight)"},
-      {name: "Low", slug: "low", description: "Low risk of oak wilt (<5% or >95% beetle flight)"},
-      {name: "Very Low", slug: "very_low", description: "Very low risk of oak wilt"}
+      {name: "Peak vector flight", slug: "very_high", description: "Very high risk of oak wilt transmission (25-75% beetle vector flight)"},
+      {name: "10-25% vector flight", slug: "high", description: "High risk of oak wilt transmission (10-25% or 75-90% beetle flight)"},
+      {name: "5-10% vector flight", slug: "medium", description: "Moderate risk of oak wilt transmission (5-10% or 90-95% beetle flight)"},
+      {name: "<5% vector flight", slug: "low", description: "Lower risk of oak wilt transmission (<5% or >95% beetle flight)"},
+      {name: "No vectors present", slug: "very_low", description: "Very low risk of oak wilt (no vectors in flight or date after July 15)"}
     ]
   end
 end
