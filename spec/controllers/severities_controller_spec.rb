be7dbe9a61486cef3d3ab442375f4324f8b2a600
Ben Bradford <bradfordium@gmail.com>
2023-11-06 12:32:08 -0600

    make sure tests check actual value

--- spec/controllers/severities_controller_spec.rb
+++ spec/controllers/severities_controller_spec.rb
@@ -2,7 +2,8 @@ require "spec_helper"
 
 RSpec.describe SeveritiesController, type: :request do
   let(:json) { JSON.parse(response.body, symbolize_names: true) }
-  let(:end_date) { Date.today }
+  let(:end_date) { "2022-7-1" }
+  let(:end_date_winter) { "2022-12-1" }
   let(:degree_days_data) {
     {
       [1, 1] => 10,
@@ -32,76 +33,100 @@ RSpec.describe SeveritiesController, type: :request do
     before do
       stub_request(:get, /degree_days\/grid*/).to_return(body: degree_days_data.to_json)
       stub_request(:get, /pest_forecasts\/grid*/).to_return(body: pest_forecast_data.to_json)
+      stub_request(:get, /weather\/freeze_grid*/).to_return(body: freeze_data.to_json)
     end
 
     describe "Disease models" do
       it "returns success response when CercosporaLeafSpot" do
         leaf_spot = CercosporaLeafSpot.create!
         allow(AgWeather).to receive(:pest_grid).and_return(pest_forecast_data)
+
         post severities_path, params: {pest_id: leaf_spot.id}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 1})
       end
 
       it "returns success response when EarlyBlight" do
         early_blight = EarlyBlight.create!
         allow(AgWeather).to receive(:pest_grid).and_return(pest_forecast_data)
-        post severities_path, params: {pest_id: early_blight.id}
+
+        post severities_path, params: {end_date:, pest_id: early_blight.id}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 0})
       end
 
       it "returns success response when FoliarDisease" do
         foliar_disease = CarrotFoliar.create!
-        allow(AgWeather).to receive(:pest_grid).and_return(pest_forecast_data)
-        post severities_path, params: {pest_id: foliar_disease.id}
+        expect(AgWeather).to receive(:pest_grid).and_return(pest_forecast_data)
+
+        post severities_path, params: {end_date:, pest_id: foliar_disease.id}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 2})
       end
 
       it "returns success response when LateBlight" do
         late_blight = LateBlight.create!
         allow(AgWeather).to receive(:pest_grid).and_return(pest_forecast_data)
-        allow(AgWeather).to receive(:freeze_days).and_return(freeze_data)
-        post severities_path, params: {pest_id: late_blight.id}
+
+        post severities_path, params: {end_date:, pest_id: late_blight.id}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 2})
       end
     end
 
     describe "Insect models" do
       it "returns success response when Insect" do
         pest = Insect.create(risk_array: [[100, 200, 300]])
-        allow(AgWeather).to receive(:dd_grid).and_return(degree_days_data)
-        allow(AgWeather).to receive(:freeze_days).and_return(freeze_data)
-        post severities_path, params: {pest_id: pest.id}
+        expect(AgWeather).to receive(:dd_grid).and_return(degree_days_data)
+
+        post severities_path, params: {end_date:, pest_id: pest.id}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 0})
       end
 
       it "returns success response when OakWilt" do
         pest = OakWilt.create!
-        allow(AgWeather).to receive(:dd_grid).and_return(degree_days_data)
-        allow(AgWeather).to receive(:freeze_days).and_return(freeze_data)
-        post severities_path, params: {pest_id: pest.id}
+        expect(AgWeather).to receive(:dd_grid).and_return(degree_days_data)
+
+        post severities_path, params: {end_date:, pest_id: pest.id}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 0})
+      end
+
+      it "calls the freeze_grid endpoint in winter" do
+        pest = Insect.create(risk_array: [[0, 10, 20]])
+        expect(AgWeather).to receive(:dd_grid).and_return(degree_days_data)
+        expect(AgWeather).to receive(:freeze_grid).and_return(freeze_data)
+
+        post severities_path, params: {end_date: end_date_winter, pest_id: pest.id}
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).not_to be_empty
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 4})
       end
     end
 
     describe "Custom tab" do
       it "returns success response when custom params sent" do
         allow(AgWeather).to receive(:dd_grid).and_return(degree_days_data)
+
         post severities_path, params: {start_date: "2020-10-01", end_date: "2020-10-10", base: "10.0", upper: "50.0"}
+
         expect(response).to have_http_status(:success)
         expect(response.body).not_to be_empty
-        expect(json.first.keys).to match([:lat, :long, :value])
+        expect(json.first).to eq({"lat": 1, "long": 1, "value": 10})
       end
     end
   end
