f155ecf3f807a23304dfe73b81669966c88407c4
Ben Bradford <bradfordium@gmail.com>
2024-01-24 10:33:05 -0600

    initial non-working update to jest v29
    jest config is not set correctly

--- app/javascript/spec/msw_mocks/handlers.js
+++ app/javascript/spec/msw_mocks/handlers.js
@@ -1,127 +1,116 @@
-import { rest } from "msw"
+import { http, HttpResponse } from "msw"
 import ENDPOINTS from "../../src/components/common/ts/endpoints"
 
 export const handlers = [
-  rest.post(ENDPOINTS.SEVERITIES, async (req, res, ctx) => {
-    const { pest_id } = await req.json()
+  http.post(ENDPOINTS.SEVERITIES, async ({ request }) => {
+    const { pest_id } = await request.json()
     if (pest_id === 101) {
-      return res(ctx.status(500))
+      return HttpResponse.json(null, { status: 500 })
     } else {
-      return res(
-        ctx.status(200),
-        ctx.json([
-          { lat: 5, long: 10, value: 10 },
-          { lat: 50, long: 60, value: 5 },
-        ]),
-      )
+      return HttpResponse.json([
+        { lat: 5, long: 10, value: 10 },
+        { lat: 50, long: 60, value: 5 },
+      ], { status: 200 })
     }
   }),
 
-  rest.get(ENDPOINTS.POINT_DETAILS, async (req, res, ctx) => {
-    const pest_id = parseInt(req.url.searchParams.get('pest_id'))
+  http.get(ENDPOINTS.POINT_DETAILS, async ({ request }) => {
+    const url = new URL(request.url);
+    const pest_id = parseInt(url.searchParams.get('pest_id'))
 
-    if (pest_id === 101) return res(ctx.status(500))
+    if (pest_id === 101) return HttpResponse.json(null, { status: 500 })
 
-    return res(ctx.status(200), ctx.json("string"))
+    return HttpResponse.json("string", { status: 200 })
   }),
 
-  rest.get(ENDPOINTS.SEVERITY_LEGEND, async (req, res, ctx) => {
-    const pest_id = parseInt(req.url.searchParams.get('pest_id'))
+  http.get(ENDPOINTS.SEVERITY_LEGEND, async ({ request }) => {
+    const url = new URL(request.url);
+    const pest_id = parseInt(url.searchParams.get('pest_id'))
 
-    if (pest_id === 101) return res(ctx.status(500))
+    if (pest_id === 101) return HttpResponse.json(null, { status: 500 })
 
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          name: "High",
-          slug: "very_high",
-          description: "High likelihood of disease",
-        },
-        {
-          name: "Medium",
-          slug: "medium",
-          description: "Medium likelihood of disease",
-        },
-        {
-          name: "Low",
-          slug: "very_low",
-          description: "Low likelihood of disease",
-        },
-      ]),
-    )
+    return HttpResponse.json([
+      {
+        name: "High",
+        slug: "very_high",
+        description: "High likelihood of disease",
+      },
+      {
+        name: "Medium",
+        slug: "medium",
+        description: "Medium likelihood of disease",
+      },
+      {
+        name: "Low",
+        slug: "very_low",
+        description: "Low likelihood of disease",
+      },
+    ], { status: 200 })
   }),
 
-  rest.get(ENDPOINTS.SEVERITY_LEGEND_INFO, async (req, res, ctx) => {
-    const pest_id = parseInt(req.url.searchParams.get('pest_id'))
+  http.get(ENDPOINTS.SEVERITY_LEGEND_INFO, async ({ request }) => {
+    const url = new URL(request.url);
+    const pest_id = parseInt(url.searchParams.get('pest_id'))
 
-    if (pest_id === 101) return res(ctx.status(500))
+    if (pest_id === 101) return HttpResponse.json(null, { status: 500 })
 
-    return res(ctx.status(200), ctx.json("Alfalfa Weevil Info"))
+    return HttpResponse.json("Alfalfa Weevil Info", { status: 200 })
   }),
 
-  rest.get(ENDPOINTS.PEST_INFO, async (req, res, ctx) => {
-    const pest_id = parseInt(req.url.searchParams.get('pest_id'))
+  http.get(ENDPOINTS.PEST_INFO, async ({ request }) => {
+    const url = new URL(request.url);
+    const pest_id = parseInt(url.searchParams.get('pest_id'))
 
-    if (pest_id === 101) return res(ctx.status(500))
+    if (pest_id === 101) return HttpResponse.json(null, { status: 500 })
 
-    return res(
-      ctx.status(200),
-      ctx.json({
-        info: "info",
-        name: "pest name",
-        pest_link: "www.example.com",
-        biofix_date: new Date().toDateString(),
-        end_date_enabled: true,
-        tmin: 0,
-        tmax: 100,
-      }),
-    )
+    return HttpResponse.json({
+      info: "info",
+      name: "pest name",
+      pest_link: "www.example.com",
+      biofix_date: new Date().toDateString(),
+      end_date_enabled: true,
+      tmin: 0,
+      tmax: 100,
+    }, { status: 200 })
   }),
 
-  rest.get(ENDPOINTS.DISEASE_PANEL, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          name: "potato",
-          diseases: [
-            { id: 1, name: "late blight" },
-            { id: 2, name: "black death" },
-          ],
-        },
-      ]),
-    )
+  http.get(ENDPOINTS.DISEASE_PANEL, () => {
+    return HttpResponse.json([
+      {
+        id: 1,
+        name: "potato",
+        diseases: [
+          { id: 1, name: "late blight" },
+          { id: 2, name: "black death" },
+        ],
+      },
+    ], { status: 200 })
   }),
 
-  rest.get(ENDPOINTS.INSECT_PANEL, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          name: "potato",
-          insects: [
-            {
-              id: 10,
-              name: "grasshopper",
-              biofix_date: "2020-10-10",
-              end_date_enabled: true,
-              t_min: 2,
-              t_max: 3,
-            },
-            {
-              id: 45,
-              name: "caterpillar",
-              biofix_date: "2020-11-10",
-              end_date_enabled: false,
-              t_min: 4,
-              t_max: 5,
-            },
-          ],
-        },
-      ]),
-    )
+  http.get(ENDPOINTS.INSECT_PANEL, () => {
+    return HttpResponse.json([
+      {
+        id: 1,
+        name: "potato",
+        insects: [
+          {
+            id: 10,
+            name: "grasshopper",
+            biofix_date: "2020-10-10",
+            end_date_enabled: true,
+            t_min: 2,
+            t_max: 3,
+          },
+          {
+            id: 45,
+            name: "caterpillar",
+            biofix_date: "2020-11-10",
+            end_date_enabled: false,
+            t_min: 4,
+            t_max: 5,
+          },
+        ],
+      },
+    ], { status: 200 })
   }),
 ]
