f51078fc5fac1cbe945a00ddbc0b513d375a11aa
Ben Bradford <bbradford@wisc.edu>
2025-04-17 12:59:34 -0500

    revert early blight

--- app/models/diseases/early_blight.rb
+++ app/models/diseases/early_blight.rb
@@ -10,16 +10,16 @@ class EarlyBlight < Disease
   end
 
   def total_to_severity(total, avg7)
-    if total >= 450
-      return 4 if avg7 > 8
+    if total >= 400
+      return 4 if avg7 > 9
       return 3 if avg7 >= 5
       return 2 if avg7 >= 3
       return 1 if avg7 >= 1
     else
-      return 4 if total >= 400
-      return 3 if total >= 350
-      return 2 if total >= 300
-      return 1 if total >= 250
+      return 4 if total >= 350
+      return 3 if total >= 300
+      return 2 if total >= 250
+      return 1 if total >= 200
     end
     0
   rescue
@@ -28,11 +28,11 @@ class EarlyBlight < Disease
 
   def severity_legend
     [
-      {value: 0, name: "Very Low", description: "Very low likelihood of disease (accumulated P-days < 250 or 7-day average < 1)"},
+      {value: 0, name: "Very Low", description: "Very low likelihood of disease (accumulated P-days < 200 or 7-day average < 1)"},
       {value: 1, name: "Low", description: "Low likelihood of disease (accumulated P-days 250-300 or 7-day average 1-3/day)"},
       {value: 2, name: "Medium", description: "Medium likelihood of disease (accumulated P-days 300-350 or 7-day average 3-5/day)"},
-      {value: 3, name: "High", description: "High likelihood of disease (accumulated P-days 350-400 or 7-day average 5-8/day)"},
-      {value: 4, name: "Very High", description: "Very high likelihood of disease (accumulated P-days 400-450 or 7-day average >8/day)"}
+      {value: 3, name: "High", description: "High likelihood of disease (accumulated P-days 350-400 or 7-day average 5-9/day)"},
+      {value: 4, name: "Very High", description: "Very high likelihood of disease (accumulated P-days >400 and 7-day average >9/day)"}
     ]
   end
 
